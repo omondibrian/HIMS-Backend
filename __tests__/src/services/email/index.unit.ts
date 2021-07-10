4172ee5e88e6d9f2b3be9fab3a6718e2c29f2753
brian <omondibrian392@gmail.com>
2021-07-10 19:13:02 +0300

    added email service

---
+++ __tests__/src/services/email/index.unit.ts
@@ -0,0 +1,28 @@
+import NotificationService, { IMail } from "../../../../src/services/email";
+import nodemailer from "nodemailer";
+
+describe("Notification service", () => {
+  const setup = async () => {
+    const test = await nodemailer.createTestAccount();
+    return test;
+  };
+
+  it("should successfully send mail", async () => {
+    const testAccount = await setup();
+    const notification = new NotificationService({
+      hostSMTP: "smtp.ethereal.email",
+      user: testAccount.user,
+      password: testAccount.pass,
+    });
+    const mail: IMail = {
+      to: "test@gmail.com",
+      from: "noreply@gmail.com",
+      body: "<H1> test msg</H1>",
+      subject: "testing",
+      text: "test msg",
+    };
+    const result = await notification.send(mail);
+    console.log(result)
+    expect(result).toBeDefined();
+  }, 100000);
+});
